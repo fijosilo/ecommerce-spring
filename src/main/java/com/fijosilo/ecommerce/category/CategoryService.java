655265b3d370870684acb5eb8ac704f3c8836dfa
Filipe Lopes <61556836+fijosilo@users.noreply.github.com>
2022-01-17 10:14:10 +0000

    Programming of the category service.

---
+++ src/main/java/com/fijosilo/ecommerce/category/CategoryService.java
@@ -0,0 +1,36 @@
+package com.fijosilo.ecommerce.category;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+    private final CategoryDAO categoryDAO;
+
+    public CategoryService(@Qualifier("JPACategoryRepository") CategoryDAO categoryDAO) {
+        this.categoryDAO = categoryDAO;
+    }
+
+    public boolean createCategory(Category category) {
+        return categoryDAO.createCategory(category);
+    }
+
+    public Category readCategoryByName(String name) {
+        return categoryDAO.readCategoryByName(name);
+    }
+
+    public List<Category> readCategories() {
+        return categoryDAO.readCategories();
+    }
+
+    public boolean updateCategory(Category category) {
+        return categoryDAO.updateCategory(category);
+    }
+
+    public boolean deleteCategory(Category category) {
+        return categoryDAO.deleteCategory(category);
+    }
+
+}
