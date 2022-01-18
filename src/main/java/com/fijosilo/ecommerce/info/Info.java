0959ab04ac46448bfad1afc962b824d33ddd87d2
Filipe Lopes <61556836+fijosilo@users.noreply.github.com>
2022-01-18 15:54:09 +0000

    Programming of the info DTO.

---
+++ src/main/java/com/fijosilo/ecommerce/info/Info.java
@@ -0,0 +1,41 @@
+package com.fijosilo.ecommerce.info;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Entity
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = "title"))
+public class Info {
+    @Id
+    @GenericGenerator(name = "increment", strategy = "increment")
+    @GeneratedValue(generator = "increment")
+    private Long id;
+    private String title;
+    private String content;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+}
